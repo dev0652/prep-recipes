2373243bdc423403462ade6afcf41d557d084bc3
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-04-30 23:47:19 +0300

    123

---
+++ src/components/Recipe.jsx
@@ -0,0 +1,27 @@
+import recipes from '../recipes.json';
+import { HiChartPie, HiChartBar } from 'react-icons/hi';
+import { TbAlarm } from 'react-icons/tb';
+
+export const Recipe = ({
+  recipe: { name, type, time, servings, calories },
+}) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <div>
+        <div>
+          <TbAlarm />
+          <p>{time} min</p>
+        </div>
+        <div>
+          <HiChartPie />
+          <p>{servings} servings</p>
+        </div>
+        <div>
+          <HiChartBar />
+          <p>{calories} calories</p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/RecipeList.jsx
@@ -0,0 +1,13 @@
+import { Recipe } from './Recipe';
+
+export const RecipeList = ({ recipes }) => {
+  return (
+    <ul>
+      {recipes.map((recipe, index) => (
+        <li key={index}>
+          <Recipe recipe={recipe} />
+        </li>
+      ))}
+    </ul>
+  );
+};
