0b27e5e314bc626f5af2de474bf8b2c3b26b2a08
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-05-11 22:26:10 +0300

    tmp

--- src/components/PaymentMethods/PaymentMethods.jsx
+++ src/components/PaymentMethods/PaymentMethods.jsx
@@ -1,48 +1,27 @@
-import React, { Component } from 'react';
 import { Button } from './PaymentMethods.styled';
+import { paymentMethods } from '../../constants/paymentMethods';
 
-const methods = Object.freeze({
-  mastercard: 'mastercard',
-  visa: 'visa',
-  applepay: 'applepay',
-});
-
-export class PaymentMethods extends Component {
-  state = {
-    selectedMethod: null,
-  };
-
-  selectPaymentMethod = method => () => {
-    this.setState({ selectedMethod: method });
-  };
-
-  render() {
-    const {
-      state: { selectedMethod },
-      selectPaymentMethod,
-    } = this;
-
-    return (
-      <>
-        <Button
-          selected={selectedMethod === methods.mastercard}
-          onCLick={selectPaymentMethod(methods.mastercard)}
-        >
-          Mastercard
-        </Button>
-        <Button
-          selected={selectedMethod === methods.visa}
-          onCLick={selectPaymentMethod(methods.visa)}
-        >
-          Visa
-        </Button>
-        <Button
-          selected={selectedMethod === methods.applepay}
-          onCLick={selectPaymentMethod(methods.applepay)}
-        >
-          Apple Pay
-        </Button>
-      </>
-    );
-  }
-}
+export const PaymentMethods = ({ selected, onSelect }) => {
+  return (
+    <>
+      <Button
+        selected={selected === paymentMethods.mastercard}
+        onCLick={() => onSelect(paymentMethods.mastercard)}
+      >
+        Mastercard
+      </Button>
+      <Button
+        selected={selected === paymentMethods.visa}
+        onCLick={() => onSelect(paymentMethods.visa)}
+      >
+        Visa
+      </Button>
+      <Button
+        selected={selected === paymentMethods.applepay}
+        onCLick={() => onSelect(paymentMethods.applepay)}
+      >
+        Apple Pay
+      </Button>
+    </>
+  );
+};
